99dd2e0b9fdb7d9ae9f348eb8c3443232b4cc4db
jwn7 <ksyzing@naver.com>
2026-05-10 16:11:21 +0900

    fix: modify router path

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -4,7 +4,7 @@ from app.api.v1.endpoints import audio, evaluate, questions, result, upload
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(upload.router, tags=["upload"])
-api_router.include_router(questions.router, tags=["questions"])
 api_router.include_router(audio.router, tags=["audio"])
+api_router.include_router(questions.router, tags=["questions"])
 api_router.include_router(evaluate.router, tags=["evaluate"])
 api_router.include_router(result.router, tags=["result"])
